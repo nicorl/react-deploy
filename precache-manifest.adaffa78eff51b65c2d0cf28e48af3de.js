1b2215a81cff27de8ee257fd61bda38fbb68951c
nicorl <nicolasrodriguezlucena@gmail.com>
2020-09-30 00:04:30 +0200

    Updates

---
+++ precache-manifest.adaffa78eff51b65c2d0cf28e48af3de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10465390688c35cb2ffbcd1179b23c",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "19fa284f50bdbd682071",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b3c218b5a63396688cd",
+    "url": "/react-deploy/static/js/2.1342e3bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.1342e3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fa284f50bdbd682071",
+    "url": "/react-deploy/static/js/main.81b2fc51.chunk.js"
+  },
+  {
+    "revision": "e6d33b005343405a69c8",
+    "url": "/react-deploy/static/js/runtime-main.693fc7c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
